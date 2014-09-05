8b0d6fb91911e833b1eb0ab1b76063d421ac336c
lucoby <lucoby@gmail.com>
2014-09-04 22:49:03 -0400

    v01

--- ExpressionParser.rb
+++ ExpressionParser.rb
@@ -1,1 +1,16 @@
-# puts "Hello world!"
+def compute_expression(expr)
+    exp_stack = []
+    while expr.length > 0
+        if expr =~ /^[\(\+\-]|^\d+/
+            sub_exp = expr.match(/^[\(\+\-]|^\d+/)
+            exp_stack.push(sub_exp)
+            expr = expr[sub_exp.length..-1]
+        else
+        end
+    end
+
+
+end
+str = "1234"
+puts "123".match(/^[\(\+\-]|^\d+/)
+puts str[1..-1]
