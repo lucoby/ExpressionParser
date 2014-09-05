d31754838c2b00f342d4204e1c3538289209f998
lucoby <lucoby@gmail.com>
2014-09-05 02:13:06 -0400

    v05
    started on parsing

---
+++ ExpressionPMParser.rb
@@ -0,0 +1,46 @@
+def compute_pme_xpression(expr)
+    exp_stack = []
+    while expr.length > 0
+        if expr =~ /^[\(\+\-]/
+            sub_exp = expr.match(/^[\(\+\-]/).to_s
+            exp_stack.push(sub_exp)
+            expr = expr[1..-1]
+        elsif expr =~ /^\d+/
+            sub_exp = expr.match(/^\d+/).to_s
+            exp_stack.push(sub_exp.to_i)
+            expr = expr[sub_exp.length..-1]
+        elsif expr =~ /^\)/
+            expr = expr[sub_exp.length..-1]
+            exp_stack = evaluate(exp_stack)
+        end
+    end
+
+    puts evaluate(exp_stack)
+end
+
+def evaluate(exp_stack)
+    eval_stack = []
+    while exp_stack.length > 0
+        eval = exp_stack.pop
+        if eval =~ /\(/
+            break
+        else
+            eval_stack.push(eval)
+        end
+    end
+    while eval_stack.length > 1
+        a = eval_stack.pop
+        op = eval_stack.pop
+        b = eval_stack.pop
+        if op =~ /\+/
+            eval_stack.push(a + b)
+        elsif op =~ /\-/
+            eval_stack.push(a - b)
+        end
+    end
+
+    return exp_stack.push(eval_stack.pop)
+end
+
+compute_pm_expression("(2+3)+((2+3-5)+3)")
+
--- ExpressionParser.rb
+++ ExpressionParser.rb
@@ -1,46 +1,11 @@
-def compute_pme_xpression(expr)
-    exp_stack = []
-    while expr.length > 0
-        if expr =~ /^[\(\+\-]/
-            sub_exp = expr.match(/^[\(\+\-]/).to_s
-            exp_stack.push(sub_exp)
-            expr = expr[1..-1]
-        elsif expr =~ /^\d+/
-            sub_exp = expr.match(/^\d+/).to_s
-            exp_stack.push(sub_exp.to_i)
-            expr = expr[sub_exp.length..-1]
-        elsif expr =~ /^\)/
-            expr = expr[sub_exp.length..-1]
-            exp_stack = evaluate(exp_stack)
-        end
-    end
-
-    puts evaluate(exp_stack)
+def getNextParse(expr)
+    parse = expr.match(/^[\+\-\^\*\/\(\)]|^\d+/).to_s
+    expr = expr[parse.length..-1]
+    return [parse, expr]
 end
 
-def evaluate(exp_stack)
-    eval_stack = []
-    while exp_stack.length > 0
-        eval = exp_stack.pop
-        if eval =~ /\(/
-            break
-        else
-            eval_stack.push(eval)
-        end
-    end
-    while eval_stack.length > 1
-        a = eval_stack.pop
-        op = eval_stack.pop
-        b = eval_stack.pop
-        if op =~ /\+/
-            eval_stack.push(a + b)
-        elsif op =~ /\-/
-            eval_stack.push(a - b)
-        end
-    end
-
-    return exp_stack.push(eval_stack.pop)
+expr = "(12+3^2)*2-2*4"
+while expr.length > 0
+    puts getNextParse(expr)[0]
+    expr = getNextParse(expr)[1]
 end
-
-compute_pm_expression("(2+3)+((2+3-5)+3)")
-
