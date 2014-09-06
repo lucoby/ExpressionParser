392f696caa7ead6b2c05151ebd9d90aded2cc039
lucoby <lucoby@gmail.com>
2014-09-05 22:48:03 -0400

    v06
    added node class and case statement

--- ExpressionParser.rb
+++ ExpressionParser.rb
@@ -1,3 +1,11 @@
+class Node
+    attr_reader :word :left :right
+    def initialize(word)
+        @word = word
+    end
+end
+
+
 def getNextParse(expr)
     parse = expr.match(/^[\+\-\^\*\/\(\)]|^\d+/).to_s
     expr = expr[parse.length..-1]
@@ -5,7 +13,19 @@ def getNextParse(expr)
 end
 
 expr = "(12+3^2)*2-2*4"
+
+priority = ["^","*","+"]
+
 while expr.length > 0
     puts getNextParse(expr)[0]
     expr = getNextParse(expr)[1]
+
+    case expr
+    when /\^/
+
+    when /[\*\/]/
+
+    when /\+\-/
+
+    end
 end
