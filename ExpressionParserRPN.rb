2e6fc1c86c974a824ded2e0db77640cb18b9b20d
lucoby <lucoby@gmail.com>
2014-09-08 20:56:14 -0400

    v11
    - RPN complete I think(?)
    - still need to work on normal expression parsing

--- ExpressionParserRPN.rb
+++ ExpressionParserRPN.rb
@@ -1,31 +1,74 @@
 def getNextParse(expr)
-    parse = expr.match(/^[\+\-\^\*\/\s]|^\d+/).to_s
+    parse = expr.match(/^[\+\-\^\*\/\s\,]|^\d+\.?\d*|^\.\d+/).to_s
     expr = expr[parse.length..-1]
     return [parse, expr]
 end
 
-expr = "12 2 +"
+expr = "14 2 + -.5 ^"
 expr_stack = Array.new
+
 while expr.length > 0
     parse = getNextParse(expr)
-    sub_exp = parse[0]
+    sub_expr = parse[0]
     expr = parse[1]
 
-    puts sub_expr
-    puts expr
+    # puts sub_expr
     case sub_expr
+    when /\-?\d+\.?\d*/
+        expr_stack.push(sub_expr.to_f)
     when /\^/
-        
+        b = expr_stack.pop
+        a = expr_stack.pop
+        if !(a.nil? || b.nil?)
+            expr_stack.push(a ** b)
+        else
+            puts "ERROR"
+        end
     when /[\*]/
-
+        b = expr_stack.pop
+        a = expr_stack.pop
+        if !(a.nil? || b.nil?)
+            expr_stack.push(a * b)
+        else
+            puts "ERROR"
+        end
     when /[\/]/
-        
+        b = expr_stack.pop
+        a = expr_stack.pop
+        if !(a.nil? || b.nil?)
+            expr_stack.push(a / b)
+        else
+            puts "ERROR"
+        end
     when /\+/
-
+        b = expr_stack.pop
+        a = expr_stack.pop
+        if !(a.nil? || b.nil?)
+            expr_stack.push(a + b)
+        else
+            puts "ERROR"
+        end
     when /[\-]/
-        
-    when /\d+/
-    
+        neg_test = getNextParse(expr)
+        if neg_test[0] =~/\d+\.?\d*|\.\d+/
+            expr = neg_test[1]
+            expr_stack.push(-(neg_test[0].to_f))
+        else
+            b = expr_stack.pop
+            a = expr_stack.pop
+            if !(a.nil? || b.nil?)
+                expr_stack.push(a - b)
+            elsif a.nil? && !b.nil?
+                expr_stack.push(-b)
+            else
+                puts "ERROR"
+            end
+        end
     end
+end
+if expr_stack.length > 2
+    puts "ERROR: not enough ops"
+else
+    puts expr_stack.pop
 end
 
